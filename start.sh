273062a7aea1e2ab0c213d98603ebd4245e51df0
ngspace <71827944+NGSpace@users.noreply.github.com>
2025-08-10 16:44:01 +0300

    Move config

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar ngsweb.jar --spring.config.location=classpath:/,file:ngsweb.yml
+java -jar ngsweb.jar --spring.config.location=classpath:/,file:config/ngsweb.yml,file:config/server.yml
